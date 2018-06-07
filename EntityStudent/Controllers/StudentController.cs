ca7e78ba8464a8e61c1fd0d3eba9b93f2541ad39
Naimisha5 <naimisha.g@neudesic.com>
2018-06-07 19:20:56 +0530

    Added EF

---
+++ EntityStudent/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityStudent.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EntityStudent.Models;
+
+namespace EntityStudent.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Student")]
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            using (var StuContext = new ContextStudent())
+            {
+                var StudentsList = new List<ModelStudent>();
+                StudentsList = StuContext.TableStudent.ToList();
+                return Ok(StudentsList);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]ModelStudent StudentObject)
+        {
+            using (var StuContext = new ContextStudent())
+            {
+                StuContext.TableStudent.Add(StudentObject);
+                StuContext.SaveChanges();
+            }
+            return Ok("StudentAdded");
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody]ModelStudent StudentObject)
+        {
+            using (var StuContext = new ContextStudent())
+            {
+                StuContext.TableStudent.Update(StudentObject);
+                StuContext.SaveChanges();
+            }
+            return Ok("StudentUpdated");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromBody]ModelStudent StudentObject)
+        {
+            using (var StuContext = new ContextStudent())
+            {
+                StuContext.TableStudent.Remove(StudentObject);
+                StuContext.SaveChanges();
+            }
+            return Ok("StudentDeleted");
+        }
+    }
+}
---
+++ EntityStudent/Data/ContextStudent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityStudent.Models;
+
+namespace EntityStudent.Data
+{
+    public class ContextStudent : DbContext
+    {
+        public DbSet<ModelStudent> TableStudent { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=NG01; Database=StudentDetails; Trusted_Connection=True; MultipleActiveResultSets=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
